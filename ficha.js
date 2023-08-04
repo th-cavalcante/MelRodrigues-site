cca24f1f66befa9fe11754f7fa0a3d07d0ddefe7
Thiago Cavalcante <th-fernandes@hotmail.com>
2023-08-04 16:00:46 -0300

    teste

--- ficha.js
+++ ficha.js
@@ -43,15 +43,43 @@ if (form) {
     const resposta1 = document.getElementById('resposta1');
     const resposta2 = document.getElementById('resposta2');
     const resposta3 = document.getElementById('resposta3');
-    //pergunta e resposta 11
+    const resposta4 = document.getElementById('resposta4');
+    const resposta5 = document.getElementById('resposta5');
+    const resposta6 = document.getElementById('resposta6');
+    const resposta7 = document.getElementById('resposta7');
+    const resposta8 = document.getElementById('resposta8');
+    const resposta9 = document.getElementById('resposta9');
+    const resposta10 = document.getElementById('resposta10');
+    //pergunta e resposta 1
     const respSelecionada1 = document.querySelector("input[name='pergunta1']:checked").value;
     resposta1.innerHTML = respSelecionada1
-     //pergunta e resposta 11
+     //pergunta e resposta 2
     const respSelecionada2 = document.querySelector("input[name='pergunta2']:checked").value;
     resposta2.innerHTML = respSelecionada2
-     //pergunta e resposta 11
+     //pergunta e resposta 3
     const respSelecionada3 = document.querySelector("input[name='pergunta3']:checked").value;
     resposta3.innerHTML = respSelecionada3
+    //pergunta e resposta 4
+    const respSelecionada4 = document.querySelector("input[name='pergunta4']:checked").value;
+    resposta4.innerHTML = respSelecionada4
+    //pergunta e resposta 5
+    const respSelecionada5 = document.querySelector("input[name='pergunta5']:checked").value;
+    resposta5.innerHTML = respSelecionada5
+    //pergunta e resposta 6
+    const respSelecionada6 = document.querySelector("input[name='pergunta6']:checked").value;
+    resposta6.innerHTML = respSelecionada6
+    //pergunta e resposta 7
+    const respSelecionada7 = document.querySelector("input[name='pergunta7']:checked").value;
+    resposta7.innerHTML = respSelecionada7
+    //pergunta e resposta 8
+    const respSelecionada8 = document.querySelector("input[name='pergunta8']:checked").value;
+    resposta8.innerHTML = respSelecionada8
+    //pergunta e resposta 9
+    const respSelecionada9 = document.querySelector("input[name='pergunta9']:checked").value;
+    resposta9.innerHTML = respSelecionada9
+    //pergunta e resposta 10
+    const respSelecionada10 = document.querySelector("input[name='pergunta10']:checked").value;
+    resposta10.innerHTML = respSelecionada10
 
     //observacoes
     const respObservation = document.getElementById('campo-observation').value;
@@ -73,64 +101,53 @@ if (form) {
 
 
 
-function GerarPdf() {
-    // Cria um novo objeto da biblioteca jspdf
-    const doc = new jsPDF();
 
-    // Define o título para o PDF
+
+
+
+function GerarPdf() {
+    // Verifica se todos os campos obrigatórios foram preenchidos
+    const name = document.getElementById("name").value;
+    const date = document.getElementById("input-date").value;
+    const rg = document.getElementById("rg").value;
+    const rua = document.getElementById("rua").value;
+    const bairro = document.getElementById("bairro").value;
+    const cidade = document.getElementById("cidade").value;
+    const cep = document.getElementById("cep").value;
+
+    if (!name || !date || !rg || !rua || !bairro || !cidade || !cep) {
+        alert('Por favor, preencha todos os campos obrigatórios antes de gerar o PDF.');
+        return;
+    }
+
+    // Verifica se todas as perguntas foram respondidas
+    const perguntasRespondidas = document.querySelectorAll('input[name^="pergunta"]:checked');
+    if (perguntasRespondidas.length < 10) {
+        alert('Por favor, responda todas as perguntas antes de gerar o PDF.');
+        return;
+    }
+
+    // Se todos os campos obrigatórios e perguntas foram preenchidos, continua com a geração do PDF
+    const doc = new jsPDF();
     doc.setFontSize(20);
     doc.text(10, 20, 'Ficha Anamnese preenchida');
 
-    // Dados pessoais
-    doc.setFontSize(14);
-    doc.text(10, 40, 'Dados pessoais:');
-    doc.setFontSize(12);
-    doc.text(20, 50, 'Nome Completo:');
-    doc.text(80, 50, document.getElementById('name-print').textContent);
-    doc.text(20, 60, 'Data de Nascimento:');
-    doc.text(80, 60, document.getElementById('date-print').textContent);
-    doc.text(20, 70, 'RG:');
-    doc.text(80, 70, document.getElementById('rg-print').textContent);
+    const perguntasRespostas = document.querySelectorAll('.respostas');
+    let yPosition = 40; // Define a posição vertical inicial
 
-    // Sexo
-    doc.text(20, 80, 'Sexo:');
-    doc.text(80, 80, document.getElementById('genero-print').textContent);
+    perguntasRespostas.forEach((item) => {
+        const pergunta = item.querySelector('.pergunta').textContent;
+        const resposta = item.querySelector('.resposta').textContent;
 
-    // Endereço
-    doc.setFontSize(14);
-    doc.text(10, 100, 'Endereço:');
-    doc.setFontSize(12);
-    doc.text(20, 110, 'Rua e nº:');
-    doc.text(80, 110, document.getElementById('rua-print').textContent);
-    doc.text(20, 120, 'Bairro:');
-    doc.text(80, 120, document.getElementById('bairro-print').textContent);
-    doc.text(20, 130, 'Cidade:');
-    doc.text(80, 130, document.getElementById('cidade-print').textContent);
-    doc.text(20, 140, 'CEP:');
-    doc.text(80, 140, document.getElementById('cep-print').textContent);
+        doc.setFontSize(12);
+        doc.text(20, yPosition, pergunta);
+        doc.text(80, yPosition, resposta);
+        yPosition += 15; // Incrementa a posição vertical para a próxima pergunta e resposta
+    });
 
-    // Questionário clínico
-    doc.setFontSize(14);
-    doc.text(10, 160, 'Questionário clínico:');
-    doc.setFontSize(12);
-    doc.text(20, 170, 'Realizou alguma cirurgia?');
-    doc.text(80, 170, document.getElementById('resposta1').textContent);
-    doc.text(20, 180, 'Costuma fazer Depilação?');
-    doc.text(80, 180, document.getElementById('resposta2').textContent);
-    doc.text(20, 190, 'Está fazendo algum tratamento dermatológico?');
-    doc.text(120, 190, document.getElementById('resposta3').textContent);
-
-    // Observações
-    doc.setFontSize(14);
-    doc.text(10, 210, 'Observações:');
-    doc.setFontSize(12);
-    doc.text(20, 220, document.getElementById('resposta-obs').textContent);
-
-    // Assinatura
-    doc.text(10, 240, 'Assinatura:');
-    doc.text(80, 240, '____________________________________________');
-
-    // Salva o PDF com o nome "ficha_anamnese.pdf"
     doc.save('ficha_anamnese.pdf');
 }
+
+
+
 
