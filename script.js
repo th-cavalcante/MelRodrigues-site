bd3f700a1401beb0dad574d78b811d576ac28609
Thiago Cavalcante <th-fernandes@hotmail.com>
2023-08-11 19:34:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ window.sr = ScrollReveal({reset: true });
 
 sr.reveal('.card-benefit', {
     rotate: { x: 0, y: 80, z: 0},
-    duration: 1500,
+    duration: 2000,
 })
 
 sr.reveal('#container-carrossel', {
